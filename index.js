2b13437fe3cb1f874b4dc8452a2e70376772228a
hars321 <54657843+hars321@users.noreply.github.com>
2021-01-07 14:57:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,11 @@ const fetch = require("node-fetch");
 
 
 // app.use(cors());
-app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Accept');
-  
+app.use(function(request, response, next) {
+response.setHeader("Access-Control-Allow-Origin", "*");
+response.setHeader("Access-Control-Allow-Credentials", "true");
+response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
 
