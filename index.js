d530d7c78eddf9621adae695eb2abeb3cee05a24
hars321 <54657843+hars321@users.noreply.github.com>
2021-02-07 20:10:40 +0530

    cors error

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const fetch = require("node-fetch");
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://fast-sea-54594.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
