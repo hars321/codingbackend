92b1beda5a03dc9fe279eff220eb6e349a9d7650
hars321 <54657843+hars321@users.noreply.github.com>
2021-02-07 20:04:19 +0530

    cors error

--- index.js
+++ index.js
@@ -11,15 +11,21 @@ const fetch = require("node-fetch");
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.use(function(req, res, next) {
-    // res.setHeader("Access-Control-Allow-Origin",'https://fast-sea-54594.herokuapp.com');
-    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Accept');
-    
-    next();
-});
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
 
 var connection = mysql.createConnection({
     host: 'remotemysql.com',
